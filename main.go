4364a8fb4486d5a64d25283412ddc2fe0bb8073c
ratogabriel <gabriel.rato@mercadolivre.com>
2020-07-22 22:13:00 -0300

    added a listener on background

--- main.go
+++ main.go
@@ -5,44 +5,79 @@ package main
 import (
 	"bufio"
 	"flag"
+	"fmt"
+	"log"
 	"net"
 	"os"
-	"fmt"
+	"strings"
 )
 
 func main() {
+	initi()
 	var dial, port string
 	flag.StringVar(&dial, "d", "no message", "send a msg")
 	flag.StringVar(&port, "p", "no message", "send a msg")
 	flag.Parse()
+
+	//connect(dial)
+	go startListen(port)
+
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("-> ")
+		text, _ := reader.ReadString('\n')
+
+		s := strings.Split(text, " ")
+		//dial = s[1]
+		var command = s[0]
+		if strings.Compare(command, "conn") == 0 {
+			connect(dial)
+		}
+}
+
+// TODO desconect a peer should not kill a host connected
+func initi() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+}
+
+func startListen(port string) {
 	service := ":" + port
 	tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
 	checkError(err)
 
 	listener, err := net.ListenTCP("tcp", tcpAddr)
 	checkError(err)
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		checkError(err)
+		// convert CRLF to LF
+		go handleClient(conn)
+	}
+}
+
+func connect(dial string) {
+	fmt.Println("connecting to... " + dial)
 
 	var caller net.Conn
 	caller, errx := net.Dial("tcp", ":"+ dial)
-
-	conn, err := listener.Accept()
-	if err != nil {
+	// accepts multi thread
+	for errx != nil {
+		//fmt.Fprintf(os.Stderr, "Fatal error: %s", errx)
+		caller, errx = net.Dial("tcp", ":"+ dial)
 	}
+	fmt.Println("connected to... " + dial)
+
 	for {
-		// accepts multi thread
-		for errx != nil {
-			fmt.Fprintf(os.Stderr, "Fatal error: %s", err)
-			caller, errx = net.Dial("tcp", ":"+ dial)
-		}
-		reader := bufio.NewReader(os.Stdin)
-
-		fmt.Print("-> ")
-		text, _ := reader.ReadString('\n')
-		// convert CRLF to LF
-		go caller.Write([]byte(text))
-		go handleClient(conn)
-
+		handleWrite(caller)
 	}
+}
+
+func handleWrite(caller net.Conn) {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("-> ")
+	text, _ := reader.ReadString('\n')
+	caller.Write([]byte(text))
 }
 
 func handleClient(conn net.Conn) {
@@ -61,7 +96,8 @@ func handleClient(conn net.Conn) {
 
 func checkError(err error) {
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		log.Print(err.Error())
+		//fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
 		os.Exit(1)
 	}
 }
